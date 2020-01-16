546b78390ef2019cfb8ac23212e1c92bc61eb167
xinrihui <735407517@qq.com>
2020-01-16 19:56:40 +0800

    update

--- 26_red_black_tree/red_black_tree_xrh.py
+++ 26_red_black_tree/red_black_tree_xrh.py
@@ -137,7 +137,7 @@ class RedBlackTree_recursive:
         """
         self.moveflipColors(h) #从父节点h中借 红链接 给两个儿子
 
-        if self.isRedNode(h.right.left): #判断兄弟节点，如果是红节点，也从兄弟节点中借一个
+        if self.isRedNode( h.right.left ): #判断兄弟节点，如果是红节点，也从兄弟节点中借一个
             h.right=self.rotateRight(h.right)
             h=self.rotateLeft(h)
             self.flipcolor(h)  # 从兄弟节点借了一个红链接以后，我们就需要 将刚刚从父节点 借来的红链接 还给父节点了
@@ -145,7 +145,8 @@ class RedBlackTree_recursive:
 
     def balance(self, x):
         """
-        保证 根节点为x 的子树下 的红链接必然是左链接，并且左右子树黑链接的高度相同
+        保证 根节点为x 的子树下 的红链接必然是左链接，
+        并且左右子树黑链接的高度相同
         :param h: 
         :return: 
         """
@@ -162,7 +163,7 @@ class RedBlackTree_recursive:
         return x
 
     def deleteMin(self):
-        if self.isRedNode(self.root)==False and self.isRedNode(self.root)==False: #如果根节点的左右子节点是2-节点，我们可以将根设为红节点，
+        if self.isRedNode(self.root.left)==False and self.isRedNode(self.root.right)==False: #如果根节点的左右子节点是2-节点，我们可以将根设为红节点，
                                                                                    # 这样才能进行后面的moveRedLeft操作，因为左子树要从根节点借一个红色链接
             self.root.set_red()
         self.root=self._deleteMin(self.root)
@@ -187,7 +188,7 @@ class RedBlackTree_recursive:
         return h
 
     def deleteMax(self):
-        if self.isRedNode(self.root)==False and self.isRedNode(self.root)==False: #如果根节点的左右子节点是2-节点，我们可以将根设为红节点，
+        if self.isRedNode(self.root.left) == False and self.isRedNode(self.root.right) == False: #如果根节点的左右子节点是2-节点，我们可以将根设为红节点，
                                                                                    # 这样才能进行后面的 moveRedRight 操作，因为左子树要从根节点借一个红色链接
             self.root.set_red()
         self.root=self._deleteMax(self.root)
@@ -200,12 +201,64 @@ class RedBlackTree_recursive:
 
         if x.right is None : return None # 没有比 x 更大的节点了 ，可以把x 删除
 
-        if self.isRedNode(x.right) == False and self.isRedNode( x.right.left) == False: #
+        if self.isRedNode(x.right) == False and self.isRedNode( x.right.left) == False:  # 判断x的 右节点是不是2-节点，因为红链接只能在左边 所以x.right.left
             x=self.moveRedRight(x)
 
         x.right=self._deleteMax(x.right)
 
         return self.balance(x) # 解除临时组成的4-节点
+
+    def delete(self,key):
+        if self.isRedNode(self.root.left) == False and self.isRedNode(self.root.right) == False:  #如果根节点的左右子节点是2-节点，我们可以将根设为红节点，
+                                                                                   # 这样才能进行后面的 moveRedRight 操作，因为左子树要从根节点借一个红色链接
+            self.root.set_red()
+        self.root=self._delete(self.root,key)
+        self.root.set_black() #借完以后，将根节点的颜色复原
+
+    def _delete(self,x,key):
+
+        if ord(key) < ord(x.key): # 当目标键小于当前键的时候，我们做类似于寻找最小键 的操作，向树的左子树查找，合并父子结点来消除2-结点
+
+            # if x.left is None: return None  # 没有比 x 更小的节点了 ，可以把x 删除
+
+            if self.isRedNode(x.left) == False and self.isRedNode(x.left.left) == False:  # 判断x的左节点是不是2-节点
+                x = self.moveRedLeft(x)
+
+            x.left = self._delete(x.left,key)
+
+        else:  #当目标键大于当前键的时候，我们向树的右子树查找，并做与deleteMax相同的操作
+
+            if self.isRedNode(x.left) == True:
+                x = self.rotateRight(x)
+
+            if ord(key) == ord(x.key) and  x.right is None: return None  # 没有比 x 更大的节点了 ，可以把x 删除
+
+            if self.isRedNode(x.right) == False and self.isRedNode(x.right.left) == False:  # 判断x的 右节点是不是2-节点，因为红链接只能在左边 所以x.right.left
+                x = self.moveRedRight(x)
+
+
+            if  ord(key) == ord(x.key) : #如果相同的话，我们使用和 BST 的删除一样的操作，获取当前键的右子树的最小健，然后交换，并将目标键删除
+                min_node=self.get_min(x.right)
+                x.key=min_node.key
+                x.val=min_node.val
+                x.right=self._deleteMin(x.right)
+
+            else:
+                x.right = self._delete(x.right,key)
+
+        return self.balance(x)
+
+    def get_min(self,root):
+        """
+        返回最小值节点
+        :return:
+        """
+        p = root
+        while p.left:
+
+            p=p.left
+        return p
+
 
     def __repr__(self):
 
@@ -285,6 +338,13 @@ if __name__ == '__main__':
 
     rbtree.deleteMax()
     print(rbtree)
+
+    rbtree.delete('S')
+    print(rbtree)
+
+    rbtree.delete('M')
+    print(rbtree)
+
 
 
 
