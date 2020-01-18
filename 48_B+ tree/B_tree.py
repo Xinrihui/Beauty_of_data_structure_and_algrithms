8557bf52f6a74ec5e3b4b4638f01807d98c9cda9
xinrihui <735407517@qq.com>
2020-01-18 20:45:47 +0800

    update

---
+++ 48_B+ tree/B_tree.py
@@ -0,0 +1,254 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+# B Tree
+# http://dieslrae.iteye.com/blog/1453853
+# http://blog.csdn.net/v_july_v/article/details/6530142
+
+class Entity(object):
+    '''''数据实体'''
+
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+
+import bisect
+
+class Node(object):
+    '''''B树的节点'''
+
+    def __init__(self):
+        self.parent = None
+        self.entitys = []
+        self.childs = []
+
+    def _find_insert_idx(self, l,v):
+        """
+        二分查找，找到有序数组的插入位置
+
+        还可以直接使用 bisect 实现：
+         http://kuanghy.github.io/2016/06/14/python-bisect
+
+        :param v:
+        :return:
+
+    test.children=[Node('b') ,Node('d'),Node('e'),Node('f')]
+    print(test._find_insert_idx( 'a'))  #[False, -1]
+    print(test._find_insert_idx('c')) #[False,0]
+    print(test._find_insert_idx('g')) #[False, 3]
+    print(test._find_insert_idx('d')) #[True, 1, Node('d')]
+        """
+        left = 0
+        right = len(l) - 1
+        flag = False
+        idx = 0
+
+        while (left <= right):
+            mid = ((left + right) // 2)  # Python 3以后  " / "就表示 浮点数除法，返回浮点结果;" // "表示整数除法。
+
+            if (l[mid].key) == v:
+                flag = True
+                idx = mid
+                break
+            elif v > (l[mid].key):
+                left = mid + 1  # mid+1 为了跳出循环
+            elif v < (l[mid].key):
+                right = mid - 1  # mid-1 为了跳出循环
+        if left > right:
+            idx = right
+            return [flag, idx]
+
+        return [flag, idx, l[idx]]
+
+    def find(self, key):
+        '''''通过key查找并返回一个数据实体 (下标,实体)  '''
+        res=self._find_insert_idx(self.entitys,key)
+        if res[0]==False:
+            return  None
+        else:
+            return res[1:]
+
+
+    def delete(self, key):
+        ''''' 通过key删除一个数据实体,并返回它和它的下标(下标,实体) '''
+        res = self._find_insert_idx(self.entitys, key)
+        if res[0]==False: # 没有找到
+            return  None
+        else:
+            del self.entitys[res[1]]
+            return  res[1:]
+
+    def isLeaf(self):
+        '''''判断该节点是否是一个叶子节点'''
+        return len(self.childs)==0
+
+
+    def addEntity(self, entity):
+        """
+        添加一个数据实体
+        并保持 entitys 的有序 
+        :param entity: 
+        :return: 
+        """
+
+        result = self._find_insert_idx(self.entitys, entity.key)
+
+        if result[0] == False:
+            idx=result[1]
+            l=self.entitys
+            if idx>=len(l)-1:
+                l.append(entity)
+            elif idx<=0:
+                l.insert(0,entity)
+            else:
+                l.insert(idx+1,entity)
+
+    def _find_insert_idx_childs(self, v):
+        """
+        二分查找，找到有序数组的插入位置
+
+        """
+        l=self.childs
+        left = 0
+        right = len(l) - 1
+        flag = False
+        idx = 0
+
+        while (left <= right):
+            mid = ((left + right) // 2)  # Python 3以后  " / "就表示 浮点数除法，返回浮点结果;" // "表示整数除法。
+
+            if (l[mid].entitys[-1] .key) == v:
+                flag = True
+                idx = mid
+                break
+            elif v > (l[mid].entitys[-1].key):
+                left = mid + 1  # mid+1 为了跳出循环
+            elif v < (l[mid].entitys[-1].key):
+                right = mid - 1  # mid-1 为了跳出循环
+        if left > right:
+            idx = right
+            return [flag, idx]
+
+        return [flag, idx, l[idx]]
+
+
+    def addChild(self, node):
+        """
+        添加一个子节点
+        并保持 childs 的 有序性
+        :param node: 
+        :return: 
+        """
+
+        result=self._find_insert_idx_childs( node.entitys[-1].key ) #
+
+        if result[0] == False:
+            idx=result[1]
+            l=self.childs
+
+            if idx>=len(l)-1:
+                l.append(node)
+            elif idx<=0:
+                l.insert(0,node)
+            else:
+                l.insert(idx+1,node)
+
+
+class BTree(object):
+        '''B树'''
+
+        def __init__(self, size=6):
+            self.size = size
+            self.root = None
+            self.length = 0
+
+        def add(self, key, value=None):
+            """
+            '插入一条数据到B树
+            :param key: 
+            :param value: 
+            :return: 
+            """
+
+
+
+
+
+        def get(self, key):
+            '''通过key查询一个数据'''
+
+            Flag,node = self.__findNode(key)
+
+            if Flag:
+                return node.find(key).value
+
+        def isEmpty(self):
+            return self.length == 0
+
+        def __findNode(self, key):
+            '''通过key值查询一个数据在哪个节点,找到就返回该节点'''
+
+            if self.root:
+                current = self.root
+                while not current.isLeaf():
+
+                    entitys= current.entitys
+                    result = current._find_insert_idx(entitys,key)
+
+                    if  result[0] : # 说明 找到节点了
+                        return  True,current
+
+                    else: # result[0] == False # 说明没找到
+                        idx = result[1]
+                        l = current.entitys
+                        if idx >= len(l) - 1: # key 比所有的entity 都大
+                            current=current.childs[-1] # 去最右边的子树中找
+                        elif idx < 0: # key 比所有的entity 都小
+                            current = current.childs[0] # 去最左边的子树中找
+                        else:
+                            current= current.childs[ idx+1 ]
+
+                # 如果已经 搜索 到了 叶子节点
+                entitys = current.entitys
+                result = current._find_insert_idx(entitys, key)
+                if  result[0] : # 说明 找到节点了
+                    return  True,current
+
+                return False,current
+
+        def delete(self, key):
+            '''通过key删除一个数据项并返回它'''
+            pass
+
+
+        def __spilt(self, node):
+            '''
+            分裂一个节点，规则为:
+            1、中间的数据项移到父节点
+            2、新建一个右兄弟节点，将中间节点右边的数据项移到新节点
+            '''
+            pass
+
+
+if __name__ == '__main__':
+
+    t = BTree(4)
+    t.add(20)
+    t.add(40)
+    t.add(60)
+    t.add(70, 'c')
+    t.add(80)
+    t.add(10)
+    t.add(30)
+    t.add(15, 'python')
+    t.add(75, 'java')
+    t.add(85)
+    t.add(90)
+    t.add(25)
+    t.add(35, 'c#')
+    t.add(50)
+    t.add(22, 'c++')
+    t.add(27)
+    t.add(32)
+
+    print (t.get(15))
